e1bb76466f0cc863840c4f64bee25e2c2aee2ede
HellGoesOn <35145123+HellGoesOn@users.noreply.github.com>
2022-10-18 12:50:33 +0300

    Добавьте файлы проекта.

---
+++ PraktRabota/AuthForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PraktRabota
+{
+    public partial class AuthForm : Form
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new RegisterForm().Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("users.txt"))
+            {
+                List<string> s = File.ReadAllLines("users.txt").ToList();
+                if (!s.Contains($"[{textBox1.Text}={textBox2.Text}]"))
+                {
+                    MessageBox.Show("Не найден пользователь", "Ошибка входа", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не найден пользователь", "Ошибка входа", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            new MainForm().Show();
+            this.Hide();
+        }
+    }
+}
---
+++ PraktRabota/RegisterForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PraktRabota
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+                    throw new Exception();
+
+                string s = $"[{textBox1.Text}={textBox2.Text}]";
+                File.WriteAllLines("users.txt", new string[] { s });
+                this.Close();
+                MessageBox.Show("Регистрация завершена.");
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные.");
+            }
+        }
+    }
+}
